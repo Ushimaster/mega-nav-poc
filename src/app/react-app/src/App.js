5b306c3cc7fa3558e64999018299c77ee515886f
CristhianJaramilloEpam <cristhian_jaramillo@epam.com>
2023-08-11 16:13:21 -0500

    Connect to api

--- src/app/react-app/src/App.js
+++ src/app/react-app/src/App.js
@@ -1,105 +1,28 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
 import Meganav from "./components/Meganav/Meganav";
 
-const App = () => {
-  
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      menuItems: null,
+    };
+  }
 
-  const menuItems = [
-    {
-      id: 1,
-      parent: null,
-      title: "My Learning",
-      link: "#",
-    },
-    {
-      id: 2,
-      parent: null,
-      title: "Live Sessions",
-      link: "#",
-    },
-    {
-      id: 3,
-      parent: null,
-      title: "Achievements",
-      link: "#",
-    },
-    {
-      id: 4,
-      parent: null,
-      title: "Inbox",
-      link: "#",
-    },
-    {
-      id: 5,
-      parent: null,
-      title: "Others",
-      link: "#",
-    },
-    {
-      id: 100,
-      parent: 1,
-      title: "Global library",
-      link: "#",
-    },
-    {
-      id: 200,
-      parent: 100,
-      title: "Courses",
-      link: "#",
-    },
-    {
-      id: 201,
-      parent: 100,
-      title: "Learning Paths",
-      link: "#",
-    },
-    {
-      id: 202,
-      parent: 100,
-      title: "Brands",
-      link: "#",
-    },
-    {
-      id: 101,
-      parent: 1,
-      title: "Content",
-      link: "#",
-    },
-    {
-      id: 300,
-      parent: 101,
-      title: "Courses",
-      link: "#",
-    },
-    {
-      id: 301,
-      parent: 101,
-      title: "Learning Paths",
-      link: "#",
-    },
-    {
-      id: 302,
-      parent: 101,
-      title: "Brands",
-      link: "#",
-    },
-  ];
-  
+  componentDidMount() {
+    fetch("https://localhost:7084/api/menu/admin/ao/learner/manager")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          menuItems: data,
+        });
+      });
+  }
 
-
-  // const [menuItems,setMenuItems]= useState();
-  // fetch("https://localhost:7084/api/menu/admin/ao/learner/manager")
-  //   .then((response) => 
-  //     response.json()
-  //   )
-  //   .then((data) => {
-  //     console.log(data);
-  //     setMenuItems(data);
-  //   });
-    
-
-  return <Meganav menuItems={menuItems} />;
-};
+  render() {
+    return <Meganav menuItems={this.state.menuItems} />;
+  }
+}
 
 export default App;
