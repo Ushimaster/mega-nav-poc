6613d73e6caf2d5bb9779a64a3d092fb87eec67a
CristhianJaramilloEpam <cristhian_jaramillo@epam.com>
2023-08-11 13:05:29 -0500

    Change theme

--- src/app/react-app/src/components/Toggle/Toggle.js
+++ src/app/react-app/src/components/Toggle/Toggle.js
@@ -7,14 +7,23 @@ const Toggle = (props) => {
 
   return (
     <div className="row">
-      <div className="col-sm-6 col-md-3 col-lg-2">
-        {(!toggles || toggles.length === 0) && <p>No options found</p>}
-        {toggles &&
-          toggles.length > 0 &&
-          toggles.map((toggle) => {
-            return <ToggleItem key={toggle.id} toggle={toggle} menuItems={props.menuItems} />;
-          })}
-      </div>
+      {(!toggles || toggles.length === 0) && (
+        <div className="col-sm-12">
+          <h5>No options found</h5>
+        </div>
+      )}
+      {toggles &&
+        toggles.length > 0 &&
+        toggles.map((toggle) => {
+          return (
+            <div
+              key={toggle.id}
+              className="col-sm-12 col-sm-6 col-md-3 col-lg-2"
+            >
+              <ToggleItem toggle={toggle} menuItems={props.menuItems} />
+            </div>
+          );
+        })}
     </div>
   );
 };
